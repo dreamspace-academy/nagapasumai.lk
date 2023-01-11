52a5dde157b69f949ccb5e53c297aaafc94695e9
vinothraj <Kanapathipillaivinothraj@gmail.com>
2023-01-12 02:09:34 +0530

    chore: initialise the website

---
+++ static/javascript/importloadpage.js
@@ -0,0 +1,5 @@
+$(function () {
+    $("#navbar").load("./loadpages/header/navbar.html");
+    $("#main").load("./loadpages/content/main.html"); 
+    $("#footer").load("./loadpages/footer/footer.html"); 
+});
