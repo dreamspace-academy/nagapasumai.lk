ebd5e0e247201c4d526c7289d601e6388aed9356
vinothraj <Kanapathipillaivinothraj@gmail.com>
2023-01-13 23:01:14 +0530

    fix: rework navbar

--- static/javascript/importloadpage.js
+++ static/javascript/importloadpage.js
@@ -1,5 +1,6 @@
 $(function () {
     $("#navbar").load("./loadpages/header/navbar.html");
+    $("#carousel").load("./loadpages/header/carousel.html");
     $("#main").load("./loadpages/content/main.html"); 
     $("#footer").load("./loadpages/footer/footer.html"); 
 });
