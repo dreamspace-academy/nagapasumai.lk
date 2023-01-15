b4e4dd28c7107bca94df63c3c1aba6f0f005f4d4
vinothraj <Kanapathipillaivinothraj@gmail.com>
2023-01-15 21:55:35 +0530

    chore: add new twmplate

--- static/javascript/importloadpage.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $("#navbar").load("./loadpages/header/navbar.html");
-    $("#carousel").load("./loadpages/header/carousel.html");
-    $("#main").load("./loadpages/content/main.html"); 
-    $("#footer").load("./loadpages/footer/footer.html"); 
-});
