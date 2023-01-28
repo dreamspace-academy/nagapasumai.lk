731d7419ce0116544da5da08ba3a74868538b3c3
Kanapathipillaivinothraj <Kanapathipillaivinothraj@gmail.com>
2023-01-28 15:32:07 +0530

    chore: create add cart product

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function ($) {
     "use strict";
-    
+
     // Dropdown on mouse hover
     $(document).ready(function () {
         function toggleNavbarMethod() {
@@ -17,8 +17,8 @@
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -28,7 +28,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -41,20 +41,20 @@
         autoplay: true,
         smartSpeed: 1000,
         responsive: {
-            0:{
-                items:2
+            0: {
+                items: 2
             },
-            576:{
-                items:3
+            576: {
+                items: 3
             },
-            768:{
-                items:4
+            768: {
+                items: 4
             },
-            992:{
-                items:5
+            992: {
+                items: 5
             },
-            1200:{
-                items:6
+            1200: {
+                items: 6
             }
         }
     });
@@ -68,17 +68,17 @@
         autoplay: true,
         smartSpeed: 1000,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:2
+            576: {
+                items: 2
             },
-            768:{
-                items:3
+            768: {
+                items: 3
             },
-            992:{
-                items:4
+            992: {
+                items: 4
             }
         }
     });
@@ -99,6 +99,80 @@
         }
         button.parent().parent().find('input').val(newVal);
     });
-    
+
 })(jQuery);
 
+// Product list
+let productList = [
+    {
+        name: 'A',
+        price: 100,
+        incart: 0
+    },
+    {
+        name: 'B',
+        price: 200,
+        incart: 0
+    },
+    {
+        name: 'C',
+        price: 300,
+        incart: 0
+    },
+    {
+        name: 'D',
+        price: 400,
+        incart: 0
+    }
+]
+
+
+// ADD Cart 
+let Cart = document.querySelectorAll(".add-cart"); /* select all add-cart classes  =>  "add to cart" button */
+
+// Add cart
+for (let i = 0; i < Cart.length; i++) {
+
+    Cart[i].addEventListener('click', AddCart); /* call-back AddCart(); */
+
+
+}
+
+// function
+function AddCart() {
+    // console.log("AddCart is Running");
+    CartNumber();/* call-back CartNumber(); */
+}
+
+let CartNumber = () => {
+    let AddCartNumber = localStorage.getItem("cartNumber");
+    AddCartNumberInt = parseInt(AddCartNumber) /* convert string to number  */
+    // console.log(AddCartNumber);
+
+    // add cart in localStorage
+    if (AddCartNumberInt) {
+        localStorage.setItem('cartNumber', AddCartNumberInt + 1);
+        document.querySelector('.card-add span').textContent =  AddCartNumberInt + 1;
+    }
+    else {
+        localStorage.setItem('cartNumber', 1);
+        document.querySelector('.card-add span').textContent = 1;
+    }
+
+    
+
+}
+
+// Default loading cart , Count in navbar
+let DefaltLoadCart = ()=> {
+    let AddCartNumber = localStorage.getItem("cartNumber");
+
+    if(AddCartNumber){
+        document.querySelector('.card-add span').textContent = AddCartNumber;
+    }
+    // else{
+    //     console.log("Empty cart list");
+    // }
+
+}
+DefaltLoadCart(); /* Default running function  */
